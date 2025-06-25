d506ae4be6af2271e0eb0ac1eb3dc7eead8a04a9
ラゲールちゃん <laguerre-chan@rageruchannoMacBook-Pro.local>
2025-06-25 23:15:08 +0900

    Permutationクラスを作った

--- Permutation.cpp
+++ Permutation.cpp
@@ -1,27 +1,122 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <initializer_list>
+#include <set>
+#include <cassert>
 
 using namespace std;
-using Permutation = vector<int>;
 
-vector<Permutation> enum_permutation(int size){
-	Permutation perm(size);
-	vector<Permutation> result;
-	for(int i=0; i<size; i++){
-		perm[i] = i+1;
+class Permutation{
+	//要素は1,2,..,nとする．
+	public:
+		//コンストラクタ
+		Permutation(vector<int>);
+		Permutation(initializer_list<int>);
+		Permutation(int size, int i, int j);
+
+		//置換への全単射
+		Permutation reverse() const;
+		Permutation inverse() const;
+		Permutation complement() const;
+		Permutation kari() const;
+
+		//code
+		vector<int> Lehmer() const;
+
+		//統計量
+		int inv() const;
+		int maj() const;
+		int comaj() const;
+
+		//演算子
+		bool operator==(const Permutation&) const;
+		bool operator<<(const Permutation&) const;
+		Permutation operator*(const Permutation&) const;
+
+		//べんり関数
+		void show() const;
+		int size() const;
+		void show_findstat(int) const;
+		void show_findmap(const Permutation &) const;
+
+		//ゲッター・セッター
+		vector<int> get_entry(void) const;
+		int get_entry(int position) const;
+
+	private:
+		vector<int> entry;
+		bool check_vector_is_permutation(vector<int>) const;
+};
+
+bool Permutation::check_vector_is_permutation(vector<int> vec) const{
+	vector<int> sorted = vec;
+	sort(sorted.begin(), sorted.end());
+	for(int i=0; i<sorted.size(); i++){
+		if(sorted[i] != i+1){
+			return false;
+		}
 	}
-	do{
-		result.push_back(perm);
-	}while(next_permutation(perm.begin(), perm.end()));
-	return result;
+	return true;
 }
 
-void pretty_show_stat(Permutation perm, int n){
+//演算子
+bool Permutation::operator==(const Permutation& other) const{
+	return this->entry == other.entry;
+}
+Permutation Permutation::operator*(const Permutation& right) const{
+	assert(this->size() == right.size());
+	vector<int> new_entry(this->size());
+	for(int i=0; i<this->size(); i++){
+		new_entry[i] = this->get_entry(right.get_entry(i)-1);
+	}
+	Permutation product(new_entry);
+	return product;
+}
+bool Permutation::operator<<(const Permutation& other) const{
+	if(this->size() != other.size()){
+		return false;
+	}
+	for(int a=1; a<=(int)(this->size())-1; a++){
+		for(int b=a+1; b<=(int)(this->size()); b++){
+			Permutation transposition(this->size(), a, b);
+			if(not (other == (*this) * transposition) ){
+				continue;
+			}
+			if(this->entry[a-1] >= this->entry[b-1]){
+				continue;
+			}
+			int check_c_flag = 0;
+			for(int c=a+1; c<b; c++){
+				if(this->entry[a-1] < this->entry[c-1] && this->entry[c-1] < this->entry[b-1]){
+					check_c_flag = 1;
+					break;
+				}
+			}
+			if(check_c_flag == 0){
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
+//べんり関数
+int Permutation::size() const{
+	return entry.size();
+}
+void Permutation::show() const{
+	for(int i=0; i<entry.size(); i++){
+		printf("%2d", entry[i]);
+	}
+	printf("\n");
+	return;
+}
+void Permutation::show_findstat(int n) const{
 	printf("[");
-	for(int i=0; i<perm.size(); i++){
-		printf("%d", perm[i]);
-		if(i < perm.size()-1){
+	for(int i=0; i<this->entry.size(); i++){
+		printf("%d", this->entry[i]);
+		if(i < this->entry.size()-1){
 			printf(",");
 		}
 	}
@@ -29,9 +124,120 @@ void pretty_show_stat(Permutation perm, int n){
 	return;
 }
 
-Permutation inverse(Permutation input_perm){
-	//要素は1,2,..,nとする．
-	vector<int> perm = input_perm;
+void Permutation::show_findmap(const Permutation &other) const{
+	printf("[");
+	for(int i=0; i<this->size(); i++){
+		printf("%d", this->entry[i]);
+		if(i < this->size()-1){
+			printf(",");
+		}
+	}
+	printf("] => [");
+	for(int i=0; i<other.size(); i++){
+		printf("%d", other.get_entry(i));
+		if(i < other.size()-1){
+			printf(",");
+		}
+	}
+	printf("]\n");
+	return;
+}
+
+//ゲッター
+vector<int> Permutation::get_entry() const{
+	return entry;
+}
+int Permutation::get_entry(int position) const{
+	return entry[position];
+}
+
+//code
+vector<int> Permutation::Lehmer() const{
+	vector<int> code(entry.size());
+	for(int i=0; i<code.size(); i++){
+		int sm = 0;
+		for(int j=0; j<i; j++){
+			sm += (entry[j] > entry[i]);
+		}
+		code[i] = sm;
+	}
+	return code;
+}
+
+//コンストラクタ
+Permutation::Permutation(vector<int> init_entry){
+	assert(check_vector_is_permutation(init_entry));
+	entry = init_entry;
+}
+Permutation::Permutation(initializer_list<int> init_list) {
+	vector<int> init_entry(init_list);
+	assert(check_vector_is_permutation(init_entry));
+	entry = init_entry;
+}
+Permutation::Permutation(int size, int i, int j){
+	assert(1 <= i && i <= size);
+	assert(1 <= j && j <= size);
+	vector<int> init_entry(size);
+	for(int i=0; i<size; i++){
+		init_entry[i] = i+1;
+	}
+	init_entry[i-1] = j;
+	init_entry[j-1] = i;
+	entry = init_entry;
+}
+
+//統計量
+int Permutation::maj() const{
+	int sum = 0;
+	for(int i=0; i<entry.size()-1; i++){
+		if(entry[i] > entry[i+1]){
+			sum += i+1;
+		}
+	}
+	return sum;
+}
+int Permutation::inv() const{
+	int sum = 0;
+	for(int i=0; i<entry.size()-1; i++){
+		for(int j=i+1; j<entry.size(); j++){
+			if(entry[i]>entry[j]){
+				sum++;
+			}
+		}
+	}
+	return sum;
+}
+int Permutation::comaj() const{
+	int sum = 0;
+	for(int i=0; i<this->size()-1; i++){
+		if(this->entry[i] > this->entry[i+1]){
+			sum += this->size()-(i+1);
+		}
+	}
+	return sum;
+}
+
+//置換への全単射
+Permutation Permutation::kari() const{
+	vector<int> perm_entry = entry;
+	for(int i=0; i<perm_entry.size(); i++){
+		if(perm_entry[i] == perm_entry.size()){
+			perm_entry[i] = 1;
+		}else{
+			perm_entry[i]++;
+		}
+	}
+	Permutation karied(perm_entry);
+	return karied;
+}
+Permutation Permutation::reverse() const{
+	vector<int> perm_entry = entry;
+	std::reverse(perm_entry.begin(), perm_entry.end());
+	Permutation reversed(perm_entry);
+	return reversed;
+}
+Permutation Permutation::inverse() const{
+	vector<int> perm = entry;
 	vector<int> inv_perm;
 	for(int i=0; i<perm.size(); i++){
 		for(int j=0; j<perm.size(); j++){
@@ -41,38 +247,39 @@ Permutation inverse(Permutation input_perm){
 			}
 		}
 	}
-	return inv_perm;
+	Permutation inversed(inv_perm);
+	return inversed;
+}
+Permutation Permutation::complement() const{
+	int size = entry.size();
+	vector<int> comp = entry;
+	for(int i=0; i<comp.size(); i++){
+		comp[i] = size - comp[i] + 1;
+	}
+	Permutation complemented(comp);
+	return complemented;
 }
 
-int comaj(Permutation p){
-	int sum = 0;
-	int size = p.size();
-	for(int i=0; i<p.size()-1; i++){
-		if(p[i] > p[i+1]){
-			sum += size-(i+1);
-		}
+
+vector<Permutation> enum_permutation(int size){
+	vector<int> entry(size);
+	vector<Permutation> result;
+	for(int i=0; i<size; i++){
+		entry[i] = i+1;
 	}
-	return sum;
+	do{
+		Permutation perm(entry);
+		result.push_back(perm);
+	}while(next_permutation(entry.begin(), entry.end()));
+	return result;
 }
 
-int maj(Permutation p){
-	int sum = 0;
-	for(int i=0; i<p.size()-1; i++){
-		if(p[i] > p[i+1]){
-			sum += i+1;
-		}
+
+int main(){
+	vector<Permutation> perms = enum_permutation(4);
+	for(Permutation perm: perms){
+		perm.show_findmap(perm.inverse());
 	}
-	return sum;
+	return 0;
 }
 
-int inv(Permutation p){
-	int sum = 0;
-	for(int i=0; i<p.size()-1; i++){
-		for(int j=i+1; j<p.size(); j++){
-			if(p[i]>p[j]){
-				sum++;
-			}
-		}
-	}
-	return sum;
-}
---
+++ permutation_document.md
@@ -0,0 +1,114 @@
+# Permutation
+
+c++で置換を扱うためのクラスである．
+置換のワード表記を，{1,...,n}を要素とするベクトルとして内部で扱っている．
+
+
+## メンバ関数（数学感強くないもの）
+### コンストラクタ
+- `Permutation(vector<int>)`
+
+受け取った配列を置換のワード表記として扱う．受け取った配列が置換になってなかったらエラーのなる．
+
+- `Permutation(initializer_list<int>)`
+
+Permutation perm{1,3,2};みたいな感じで置換を定義する．引数を置換のワード表記として扱います．受け取ったものが置換になってなかったらエラーになる．
+
+- `Permutation(int size, int i, int j)`
+
+互換を定義できます．サイズがsizeで，iとjだけ入れ替える置換を定義する．
+1<= i,j <= sizeであることは内部でチェックされ，違反したらエラーになる．
+
+
+### ゲッター
+
+- `get_entry() const`
+
+置換のワード表記をベクトルとして返す．
+
+返り値：`vector<int>`
+
+- `get_entry(int i) const`
+
+置換をσとすると，σ(i+1)を返す．要するに，内部では置換を`vector<int>`型として扱ってるので，それの第i要素を返してるだけである．
+
+返り値：`int`
+### その他便利な関数
+
+- `show() const`
+
+置換をワード表記で表示する．
+
+- `size() const`
+
+置換のサイズを返す．
+
+返り値：`int`
+
+- `show_findstat(int n) const`
+
+`[2,3,1,4] => 2`みたいな感じで表示する関数．
+「[置換] => n」という文字列が出力される．置換の統計量を検索するサイトfindstatに入力しやすいように出力するための関数である．
+
+- `show_findmap(const Permutation & other) const`
+
+`[2,3,4,1] => [4,1,2,3]`みたいな感じで出力する関数．
+「[自分] => [other]」という文字列が出力される．置換の統計量を検索するサイトfindstatにあるmapfinderに入力しやすいように出力するための関数である．
+
+## メンバ関数（数学感強いもの）
+### 演算子
+- `operator==(const Permutation&) const`
+
+比較演算子．置換として完全に一致するかを比較する．
+
+返り値：`bool`
+
+- `operator*(const Permutation&) const`
+
+置換の合成．対称群での積を計算する．
+
+返り値：`Permutation`
+
+- `operator<<(const Permutation&) const`
+
+Bruhat順序での被覆関係．x<<yは，yがxをBruhat順序の意味で被覆することを意味する．
+
+返り値：`bool`  
+
+### 統計量
+
+- `inv() const`
+- `maj() const`
+- `comaj() const`
+
+それぞれ，文字通りの統計量を返す．
+
+返り値：`int`
+### 置換への全単射
+
+- `reverse() const`
+- `inverse() const`
+- `complement() const`
+
+名前の通りである．これらは総称してtrivial bijectionとも呼ばれる．
+
+返り値：`Permutation`
+
+### Subexcedentへの全単射
+
+
+
+- `Lehmer() const`
+
+Lehmer codeを出力する．
+
+返り値：`vector<int>` 
+
+
+# 関連する関数
+
+- `enum_permutation(int size)`
+
+大きさsizeの置換を列挙する関数．
+
+返り値：`vector<Permutation>`
